e23865278fb25686a45d46d3654442feec79bedb
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-03-20 09:17:24 +0300

    syntax change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@
     return (
         <section className="w-full nav-height bg-black relative">
         <div className="h-5/6 w-full flex-center flex-col">
-            <p id="hero" className="hero-title">Javoh's Iphone</p>
+            <p id="hero" className="hero-title">Iphone</p>
             <div className="md:w-10/12 w-9/12">
             <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
                 <source src={videoSrc} type="video/mp4" />
