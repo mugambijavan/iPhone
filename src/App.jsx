41fcab58f9862bee59316fbca6ae239ebaa2ee9d
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-03-22 18:39:11 +0300

    models

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
-import Model from "./components/Model"
+
+import * as Sentry from '@sentry/react'
 
 const App =() => {
   return (
@@ -9,9 +10,8 @@ const App =() => {
       <Navbar />
       <Hero />
       <Highlights />
-      <Model />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
