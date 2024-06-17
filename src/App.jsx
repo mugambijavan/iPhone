9efcdff622f46f87dc31981eb5d1db4b37a6a3b2
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-06-17 15:31:01 +0300

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
-import * as Sentry from '@sentry/react'
+import * as Sentry from '@sentry/react';
 
-const App =() => {
+const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
